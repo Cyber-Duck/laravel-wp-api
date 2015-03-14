3b668336f090f27f807d0807ce63b5997da4e40c
Ben Speakman <ben@cyber-duck.co.uk>
2015-03-14 16:02:53 +0000

    Add facade file, fixes #1

---
+++ src/Cyberduck/LaravelWpApi/Facades/WpApi.php
@@ -0,0 +1,9 @@
+<?php namespace Cyberduck\LaravelWpApi\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class WpApi extends Facade {
+
+    protected static function getFacadeAccessor() { return 'wp-api'; }
+
+}
